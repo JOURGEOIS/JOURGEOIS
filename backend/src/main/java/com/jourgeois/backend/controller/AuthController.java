c5aeb87454aa7fd961471b2e6488cb9262d9df23
seona98 <seona0667@naver.com>
2022-07-22 17:28:44 +0900

    [S07P12B102-116] Feat. 회원관리: 토큰 재발급

---
+++ backend/src/main/java/com/jourgeois/backend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.jourgeois.backend.controller;
+
+import com.jourgeois.backend.api.dto.TokenResponseDto;
+import com.jourgeois.backend.service.TokenService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class AuthController {
+    private  final TokenService tokenService;
+
+    @Autowired
+    AuthController(TokenService tokenService){
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/auth/token")
+    public TokenResponseDto reissueAccessToken(@RequestHeader String refreshToken){
+        System.out.println("token: " + refreshToken);
+        return tokenService.reissueAccessToken(refreshToken);
+    }
+}

5471a593e39e42491b2e02f922cd37adbfabd0e4
JK <worudp12@gmail.com>
2022-07-22 15:56:04 +0900

    [S07P12B102-114] Feat. 기본 컴포넌트 구현: 성공 알럿 팝업 컴포넌트 구현

---
+++ frontend/src/components/modals/SuccessPopUp.vue
@@ -0,0 +1,125 @@
+<template>
+  <modal-pop-up modal-color="primary">
+    <div class="success-popup-content">
+      <div class="check-icon">
+        <div class="icon-line line-left"></div>
+        <div class="icon-line line-right"></div>
+      </div>
+      <slot></slot>
+    </div>
+  </modal-pop-up>
+</template>
+
+<script setup lang="ts">
+import ModalPopUp from "@/components/basics/ModalPopUp.vue";
+</script>
+<style scoped lang="scss">
+@keyframes icon-line-left {
+  0% {
+    width: 0px;
+  }
+  50% {
+    width: 20px;
+  }
+}
+
+@keyframes icon-line-right {
+  0% {
+    width: 0px;
+  }
+  49% {
+    width: 0px;
+  }
+  50% {
+    left: 18px;
+    bottom: 3.5px;
+    width: 1px;
+  }
+  60% {
+    left: 17px;
+    bottom: 4px;
+    width: 5px;
+  }
+
+  65% {
+    left: 16.5px;
+    bottom: 5px;
+    width: 8px;
+  }
+
+  70% {
+    left: 16px;
+    bottom: 6px;
+    width: 11px;
+  }
+
+  75% {
+    left: 16px;
+    bottom: 7px;
+    width: 14px;
+  }
+
+  80% {
+    left: 15px;
+    bottom: 8px;
+    width: 17px;
+  }
+
+  85% {
+    left: 15px;
+    bottom: 9.5px;
+    width: 20px;
+  }
+
+  90% {
+    left: 14.5px;
+    bottom: 10.5px;
+    width: 23px;
+  }
+
+  95% {
+    left: 14px;
+    bottom: 12px;
+    width: 27px;
+  }
+
+  100% {
+    left: 13.5px;
+    bottom: 13px;
+    width: 30px;
+  }
+}
+
+.success-popup-content {
+  @include flex-center();
+  gap: 16px;
+}
+
+.check-icon {
+  position: relative;
+  width: 39px;
+  height: 39px;
+
+  .icon-line {
+    position: absolute;
+    height: 5px;
+    border-radius: 2px;
+    background-color: $white;
+
+    &.line-left {
+      bottom: 8.5px;
+      left: 3.5px;
+      width: 20px;
+      transform: rotate(45deg);
+      animation: icon-line-left 0.5s;
+    }
+    &.line-right {
+      left: 13.5px;
+      bottom: 13px;
+      width: 30px;
+      transform: rotate(-45deg);
+      animation: icon-line-right 0.5s;
+    }
+  }
+}
+</style>

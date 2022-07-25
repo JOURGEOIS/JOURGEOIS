b4c040c423547a46273122cf35921bbf08a1095d
박재경 <worudp12@gmail.com>
2022-07-25 10:15:38 +0900

    [S07P12B102-67] Feat. 개인정보 수정: 비밀번호 재설정 완료git add .!!

--- frontend/src/store/accounts/password.ts
+++ frontend/src/store/accounts/password.ts
@@ -7,216 +7,218 @@ import { clickHome } from "../../modules/clickEvent";
 
 // Signup의 State 타입을 설정한다.
 export interface PasswordState {
-	forgotPwCurrentTab: number;
-	forgotPwEmail: string;
-	forgotPwErrorMsgStatus: boolean;
-	changePwCurrentTab: number;
-	changePwPopupStatus: boolean;
+  forgotPwCurrentTab: number;
+  forgotPwEmail: string;
+  forgotPwErrorMsgStatus: boolean;
+  changePwCurrentTab: number;
+  changePwPopupStatus: boolean;
 }
 
 export const password: Module<PasswordState, RootState> = {
-	namespaced: true,
+  namespaced: true,
 
-	state: {
-		// 비밀번호 찾기 컴포넌트 index
-		forgotPwCurrentTab: 0,
+  state: {
+    // 비밀번호 찾기 컴포넌트 index
+    forgotPwCurrentTab: 0,
 
-		// 비밀번호 찾기에서 사용되는 이메일
-		forgotPwEmail: "",
+    // 비밀번호 찾기에서 사용되는 이메일
+    forgotPwEmail: "",
 
-		// 비밀번호 찾기 에러 메시지 status
-		forgotPwErrorMsgStatus: false,
+    // 비밀번호 찾기 에러 메시지 status
+    forgotPwErrorMsgStatus: false,
 
-		// 비밀번호 변경 컴포넌트 index
-		changePwCurrentTab: 0,
+    // 비밀번호 변경 컴포넌트 index
+    changePwCurrentTab: 0,
 
-		// 비밀번호 변경 성공 팝업
-		changePwPopupStatus: false,
-	},
+    // 비밀번호 변경 성공 팝업
+    changePwPopupStatus: false,
+  },
 
-	getters: {
-		getForgotPwCurrentTab: (state) => {
-			return state.forgotPwCurrentTab;
-		},
-		getForgotPwEmail: (state) => {
-			return state.forgotPwEmail;
-		},
-		getForgotPwErrorMsgStatus: (state) => {
-			return state.forgotPwErrorMsgStatus;
-		},
-		getChangePwCurrentTab: (state) => {
-			return state.changePwCurrentTab;
-		},
-		getChangePwPopupStatus: (state) => {
-			return state.changePwPopupStatus;
-		},
-	},
+  getters: {
+    getForgotPwCurrentTab: (state) => {
+      return state.forgotPwCurrentTab;
+    },
+    getForgotPwEmail: (state) => {
+      return state.forgotPwEmail;
+    },
+    getForgotPwErrorMsgStatus: (state) => {
+      return state.forgotPwErrorMsgStatus;
+    },
+    getChangePwCurrentTab: (state) => {
+      return state.changePwCurrentTab;
+    },
+    getChangePwPopupStatus: (state) => {
+      return state.changePwPopupStatus;
+    },
+  },
 
-	mutations: {
-		SET_FORGOT_PW_CURRENT_TAB: (state, value: number) => {
-			state.forgotPwCurrentTab = value;
-		},
-		SET_FORGOT_PW_EMAIL: (state, value: string) => {
-			state.forgotPwEmail = value;
-		},
-		SET_FORGOT_PW_ERROR_MSG: (state, value: boolean) => {
-			state.forgotPwErrorMsgStatus = value;
-		},
-		SET_CHANGE_PW_CURRENT_TAB: (state, value: number) => {
-			state.changePwCurrentTab = value;
-		},
-		SET_CHANGE_PW_POPUP: (state, value: boolean) => {
-			state.changePwPopupStatus = value;
-		},
-	},
+  mutations: {
+    SET_FORGOT_PW_CURRENT_TAB: (state, value: number) => {
+      state.forgotPwCurrentTab = value;
+    },
+    SET_FORGOT_PW_EMAIL: (state, value: string) => {
+      state.forgotPwEmail = value;
+    },
+    SET_FORGOT_PW_ERROR_MSG: (state, value: boolean) => {
+      state.forgotPwErrorMsgStatus = value;
+    },
+    SET_CHANGE_PW_CURRENT_TAB: (state, value: number) => {
+      state.changePwCurrentTab = value;
+    },
+    SET_CHANGE_PW_POPUP: (state, value: boolean) => {
+      state.changePwPopupStatus = value;
+    },
+  },
 
-	actions: {
-		changeForgotPwCurrentTab: ({ commit }, value: number) => {
-			commit("SET_FORGOT_PW_CURRENT_TAB", value);
-		},
+  actions: {
+    changeForgotPwCurrentTab: ({ commit }, value: number) => {
+      commit("SET_FORGOT_PW_CURRENT_TAB", value);
+    },
 
-		changeForgotPwEmail: ({ commit }, value: string) => {
-			commit("SET_FORGOT_PW_EMAIL", value);
-		},
+    changeForgotPwEmail: ({ commit }, value: string) => {
+      commit("SET_FORGOT_PW_EMAIL", value);
+    },
 
-		toggleForgotPwErrorMsg: ({ commit }, value: boolean) => {
-			commit("SET_FORGOT_PW_ERROR_MSG", value);
-		},
+    toggleForgotPwErrorMsg: ({ commit }, value: boolean) => {
+      commit("SET_FORGOT_PW_ERROR_MSG", value);
+    },
 
-		changePwCurrentTab: ({ commit }, value: number) => {
-			commit("SET_CHANGE_PW_CURRENT_TAB", value);
-		},
+    changePwCurrentTab: ({ commit }, value: number) => {
+      commit("SET_CHANGE_PW_CURRENT_TAB", value);
+    },
 
-		toggleChangePwPopup: ({ commit }, value: boolean) => {
-			commit("SET_CHANGE_PW_POPUP", value);
-		},
+    toggleChangePwPopup: ({ commit }, value: boolean) => {
+      commit("SET_CHANGE_PW_POPUP", value);
+    },
 
-		// 비밀번호 찾기: 이름, 이메일을 입력하여 1. 서버에 존재하는 이메일과 이름인지 2. 둘이 매치가 되는지 확인한다.
-		submitForgotPwForm: ({ commit }, { userId, userName }) => {
-			axios({
-				url: drf.accounts.forgotPassword(),
-				method: "get",
-				params: {
-					userId,
-					userName,
-				},
-			})
-				.then(() => {
-					// 다음 페이지로 이동, 이메일 정보 저장
-					commit("SET_FORGOT_PW_CURRENT_TAB", 1);
-					commit("SET_FORGOT_PW_EMAIL", userId);
-				})
-				.catch(() => {
-					// 에러 메시지 하단에 배치
-					commit("SET_FORGOT_PW_ERROR_MSG", true);
-				});
-		},
+    // 비밀번호 찾기: 이름, 이메일을 입력하여 1. 서버에 존재하는 이메일과 이름인지 2. 둘이 매치가 되는지 확인한다.
+    submitForgotPwForm: ({ commit }, { userId, userName }) => {
+      axios({
+        url: drf.accounts.forgotPassword(),
+        method: "get",
+        params: {
+          userId,
+          userName,
+        },
+      })
+        .then(() => {
+          // 다음 페이지로 이동, 이메일 정보 저장
+          commit("SET_FORGOT_PW_CURRENT_TAB", 1);
+          commit("SET_FORGOT_PW_EMAIL", userId);
+        })
+        .catch(() => {
+          // 에러 메시지 하단에 배치
+          commit("SET_FORGOT_PW_ERROR_MSG", true);
+        });
+    },
 
-		// 비밀번호 찾기: 본인확인을 위해 이메일을 발송한다.
-		submitForgotPwAuthForm: ({ getters }, payload) => {
-			const { btnStatus, loadingStatus, authFailError } = payload;
-			axios({
-				url: drf.email.emailCert(),
-				method: "post",
-				data: {
-					email: getters.getForgotPwEmail,
-				},
-			})
-				.then(() => {
-					// 버튼 섹션 생성, 로딩창 끄기
-					btnStatus.value = true;
-					loadingStatus.value = false;
-				})
-				.catch(() => {
-					// 에러메시지 하단에 배치
-					authFailError.value = true;
-					loadingStatus.value = false;
-				});
-		},
+    // 비밀번호 찾기: 본인확인을 위해 이메일을 발송한다.
+    submitForgotPwAuthForm: ({ getters }, payload) => {
+      const { btnStatus, loadingStatus, authFailError } = payload;
+      axios({
+        url: drf.email.emailCert(),
+        method: "post",
+        data: {
+          email: getters.getForgotPwEmail,
+        },
+      })
+        .then(() => {
+          // 버튼 섹션 생성, 로딩창 끄기
+          btnStatus.value = true;
+          loadingStatus.value = false;
+        })
+        .catch(() => {
+          // 에러메시지 하단에 배치
+          authFailError.value = true;
+          loadingStatus.value = false;
+        });
+    },
 
-		// 비밀번호 찾기: 이메일 인증이 되었는지 확인한다.
-		confirmAuthEmail: ({ commit, getters }, errorStatus) => {
-			axios({
-				url: drf.email.emailConfirmed(),
-				method: "post",
-				data: {
-					email: getters.getForgotPwEmail,
-				},
-			})
-				.then(() => {
-					// 다음 페이지로 이동
-					commit("SET_FORGOT_PW_CURRENT_TAB", 2);
-				})
-				.catch(() => {
-					// 에러 메시지 하단에 배치
-					errorStatus.value = true;
-				});
-		},
+    // 비밀번호 찾기: 이메일 인증이 되었는지 확인한다.
+    confirmAuthEmail: ({ commit, getters }, errorStatus) => {
+      axios({
+        url: drf.email.emailConfirmed(),
+        method: "post",
+        data: {
+          email: getters.getForgotPwEmail,
+        },
+      })
+        .then(() => {
+          // 다음 페이지로 이동
+          commit("SET_FORGOT_PW_CURRENT_TAB", 2);
+        })
+        .catch(() => {
+          // 에러 메시지 하단에 배치
+          errorStatus.value = true;
+        });
+    },
 
-		// 비밀번호 찾기 : 비밀번호 변경
-		submitChangePwForm: (context, data) => {
-			const { userId, passwordNew, passwordConfirm, failStatus } = data;
-			axios({
-				url: drf.accounts.forgotPassword(),
-				method: "put",
-				data: {
-					userId,
-					passwordNew,
-					passwordConfirm,
-				},
-			})
-				.then((response) => {
-					// 홈으로 이동
-					clickHome();
-				})
-				.catch(() => {
-					// 에러 메시지 하단에 배치
-					failStatus.value = true;
-				});
-		},
+    // 비밀번호 찾기 : 비밀번호 변경
+    submitChangePwForm: (context, data) => {
+      const { userId, passwordNew, passwordConfirm, failStatus } = data;
+      axios({
+        url: drf.accounts.forgotPassword(),
+        method: "put",
+        data: {
+          userId,
+          passwordNew,
+          passwordConfirm,
+        },
+      })
+        .then((response) => {
+          // 홈으로 이동
+          clickHome();
+        })
+        .catch(() => {
+          // 에러 메시지 하단에 배치
+          failStatus.value = true;
+        });
+    },
 
-		// 비밀번호 변경: 본인 인증
-		submitChangePwAuthForm: ({ rootGetters, commit }, data) => {
-			const { pwInputValue, failStatus } = data;
-			axios({
-				url: drf.accounts.changePassword(),
-				method: "post",
-				headers: {
-					Authorization: rootGetters["personalInfo/getAccessToken"],
-				},
-				data: {
-					userId: rootGetters["personalInfo/getUserInfoId"],
-					passwordOld: pwInputValue.value,
-				},
-			})
-				.then(() => {
-					commit("SET_CHANGE_PW_CURRENT_TAB", 1);
-				})
-				.catch((error) => {
-					failStatus.value = true;
-				});
-		},
-		// 비밀번호 변경: 비밀번호 변경
-		submitPwForm: ({ rootGetters, commit }, data) => {
-			const { passwordNew, passwordConfirm, failStatus } = data;
-			axios({
-				url: drf.accounts.changePassword(),
-				method: "put",
-				headers: {
-					Authorization: rootGetters["personalInfo/getAccessToken"],
-				},
-				data: {
-					passwordNew,
-					passwordConfirm,
-				},
-			})
-				.then((res) => {
-					clickHome();
-					commit("SET_CHANGE_PW_POPUP", true);
-				})
-				.catch((err) => {
-					failStatus.value = true;
-				});
-		},
-	},
+    // 비밀번호 변경: 본인 인증
+    submitChangePwAuthForm: ({ rootGetters, commit }, data) => {
+      const { pwInputValue, failStatus } = data;
+      axios({
+        url: drf.accounts.changePassword(),
+        method: "post",
+        headers: {
+          Authorization: rootGetters["personalInfo/getAccessToken"],
+        },
+        data: {
+          userId: rootGetters["personalInfo/getUserInfoId"],
+          passwordOld: pwInputValue.value,
+        },
+      })
+        .then(() => {
+          commit("SET_CHANGE_PW_CURRENT_TAB", 1);
+        })
+        .catch((error) => {
+          failStatus.value = true;
+        });
+    },
+    // 비밀번호 변경: 비밀번호 변경
+    submitPwForm: ({ rootGetters, commit }, data) => {
+      const { passwordNew, passwordConfirm, failStatus } = data;
+      axios({
+        url: drf.accounts.changePassword(),
+        method: "put",
+        headers: {
+          Authorization: rootGetters["personalInfo/getAccessToken"],
+        },
+        data: {
+          userId: rootGetters["personalInfo/getUserInfoId"],
+          passwordNew,
+          passwordConfirm,
+        },
+      })
+        .then(() => {
+          clickHome();
+          commit("SET_CHANGE_PW_POPUP", true);
+        })
+        .catch((err) => {
+          console.log(err.data);
+          failStatus.value = true;
+        });
+    },
+  },
 };

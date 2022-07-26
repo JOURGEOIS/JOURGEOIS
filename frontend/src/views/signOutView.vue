21d79adc9d4fbce33c2e6ac7f5c8f80d8568322f
JK <worudp12@gmail.com>
2022-07-27 00:23:11 +0900

    [S07P12B102-188] Feat. 회원탈퇴: 회원탈퇴 view 구현 완료

--- frontend/src/views/signOutView.vue
+++ frontend/src/views/signOutView.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="sign-out-view">
+    <!-- 헤더 -->
     <header-basic :prev="true" :success="false" @prevClicked="$router.go(-1)">
       회원 탈퇴
     </header-basic>
+
+    <!-- 동적 컴포넌트로 페이지를 구성한다.  -->
     <section class="sign-out-section">
-      <sign-out-check> </sign-out-check>
+      <component :is="currentComponent"></component>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import HeaderBasic from "@/components/basics/HeaderBasic.vue";
-import SignOutCheck from "@/components/accounts/SignOutCheck.vue";
+import { computed, defineAsyncComponent, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+// 현재 탭
+const index = computed(() => store.getters["account/getSignOutCurrentTab"]);
+const componentArray = [
+  defineAsyncComponent(
+    () => import("@/components/accounts/TheSignOutAuthForm.vue")
+  ),
+  defineAsyncComponent(
+    () => import("@/components/accounts/TheSignOutCheck.vue")
+  ),
+];
+const currentComponent = computed(() => {
+  return componentArray[index.value];
+});
+
+// vuex 리셋하기 (현재 탭)
+const changeCurrentComponent = (value: number) =>
+  store.dispatch("account/changeSignOutCurrentTab", value);
+onBeforeMount(() => {
+  changeCurrentComponent(0);
+});
 </script>
 
 <style scoped lang="scss">

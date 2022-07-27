586c61606feeee5dbb2099f82f099274b3733630
SSAFY <seona0667@naver.com>
2022-07-27 17:27:50 +0900

    [S07P12B102-220] Fix. Token 인증 이슈 해결

--- backend/src/main/java/com/jourgeois/backend/security/jwt/JwtTokenFilter.java
+++ backend/src/main/java/com/jourgeois/backend/security/jwt/JwtTokenFilter.java
@@ -7,15 +7,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @Slf4j
-public class JwtTokenFilter extends OncePerRequestFilter {
 
+public class JwtTokenFilter extends OncePerRequestFilter{
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String REFRESH_HEADER = "Refresh";
 
@@ -25,23 +27,18 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-
-    // controller 들어가기 전 인증 확인
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String jwt = resolveToken(request, AUTHORIZATION_HEADER);
+        String jwt = jwtTokenProvider.resolveToken(request, AUTHORIZATION_HEADER);
         try{
             if ( jwt != null && jwtTokenProvider.validateToken(jwt)) {
                 Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
-                if(request.getParameter("email") != null && request.getParameter("email").equals(authentication.getName())){
-//                    System.out.println("TESTTESTTEST " + authentication.getName() + " " + request.getParameter("email"));
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                    log.info("set Authentication to security context for '{}', uri: {}", authentication.getName(), request.getRequestURI());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("set Authentication to security context for '{}', uri: {}", authentication.getName());
                 }else {
-                    System.out.println("토큰과 이메일이 같지 않음");
                     request.setAttribute("exception", "Exception");
                 }
-            }
+//            }
         } catch(ExpiredJwtException e){
             request.setAttribute("exception", "ExpiredJwt");
             log.info("ExpiredJwtException {}", e.getMessage());
@@ -52,15 +49,4 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
-
-    // token 검증
-    private String resolveToken(HttpServletRequest request, String header) {
-        String bearerToken = request.getHeader(header);
-        if (bearerToken != null && bearerToken.startsWith("Bearer-")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-
-
 }

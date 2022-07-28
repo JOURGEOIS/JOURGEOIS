c95d97731f44cf799c5842121e46122669a48840
박승훈 <tmdgns1126@naver.com>
2022-07-28 22:51:39 +0900

    [feat/S07P12B102-224] Feat. 개발환경설정: 전역변수 설정

--- frontend/src/store/accounts/personalInfo.ts
+++ frontend/src/store/accounts/personalInfo.ts
@@ -1,188 +1,188 @@
 import axios from "axios";
 import { Module } from "vuex";
-import drf from "../../api/drf";
+import api from "../../api/api";
 import { clickHome } from "../../functions/clickEvent";
 import { RootState } from "../index";
 
 export interface userInfo {
-  [props: string]: string;
+	[props: string]: string;
 }
 
 export interface PersonalInfoState {
-  accessToken: string;
-  refreshToken: string;
-  userInfo: userInfo;
-  refreshFailPopupStatus: boolean;
+	accessToken: string;
+	refreshToken: string;
+	userInfo: userInfo;
+	refreshFailPopupStatus: boolean;
 }
 
 export const personalInfo: Module<PersonalInfoState, RootState> = {
-  namespaced: true,
+	namespaced: true,
 
-  state: {
-    accessToken: localStorage.getItem("accessToken") || "",
-    refreshToken: localStorage.getItem("refreshToken") || "",
-    userInfo: JSON.parse(localStorage.getItem("userInfo") || "{}") || {},
-    refreshFailPopupStatus: false,
-  },
+	state: {
+		accessToken: localStorage.getItem("accessToken") || "",
+		refreshToken: localStorage.getItem("refreshToken") || "",
+		userInfo: JSON.parse(localStorage.getItem("userInfo") || "{}") || {},
+		refreshFailPopupStatus: false,
+	},
 
-  getters: {
-    isLoggedIn: (state) => {
-      return !!state.accessToken;
-    },
-    getAccessToken: (state) => {
-      return state.accessToken;
-    },
-    getRefreshToken: (state) => {
-      return state.refreshToken;
-    },
-    getUserInfo: (state) => {
-      return state.userInfo;
-    },
-    getUserInfoId: (state) => {
-      return state.userInfo.email;
-    },
-    getRefreshFailPopupStatus: (state) => {
-      return state.refreshFailPopupStatus;
-    },
-  },
+	getters: {
+		isLoggedIn: (state) => {
+			return !!state.accessToken;
+		},
+		getAccessToken: (state) => {
+			return state.accessToken;
+		},
+		getRefreshToken: (state) => {
+			return state.refreshToken;
+		},
+		getUserInfo: (state) => {
+			return state.userInfo;
+		},
+		getUserInfoId: (state) => {
+			return state.userInfo.email;
+		},
+		getRefreshFailPopupStatus: (state) => {
+			return state.refreshFailPopupStatus;
+		},
+	},
 
-  mutations: {
-    SET_ACCESS_TOKEN: (state, token) => {
-      state.accessToken = token;
-    },
-    SET_REFRESH_TOKEN: (state, token) => {
-      state.refreshToken = token;
-    },
-    SET_USER_INFO: (state, data) => {
-      state.userInfo = data;
-    },
-    SET_REFRESH_FAIL: (state, value) => {
-      state.refreshFailPopupStatus = value;
-    },
-  },
+	mutations: {
+		SET_ACCESS_TOKEN: (state, token) => {
+			state.accessToken = token;
+		},
+		SET_REFRESH_TOKEN: (state, token) => {
+			state.refreshToken = token;
+		},
+		SET_USER_INFO: (state, data) => {
+			state.userInfo = data;
+		},
+		SET_REFRESH_FAIL: (state, value) => {
+			state.refreshFailPopupStatus = value;
+		},
+	},
 
-  actions: {
-    saveToken: ({ commit }, { accessToken, refreshToken }) => {
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-      commit("SET_ACCESS_TOKEN", accessToken);
-      commit("SET_REFRESH_TOKEN", refreshToken);
-    },
-    removeToken: ({ commit }) => {
-      localStorage.setItem("accessToken", "");
-      localStorage.setItem("refreshToken", "");
-      commit("SET_ACCESS_TOKEN", "");
-      commit("SET_REFRESH_TOKEN", "");
-    },
-    saveUserInfo: ({ commit }, data: object) => {
-      const jsonUserInfo = JSON.stringify(data);
-      localStorage.setItem("userInfo", jsonUserInfo);
-      commit("SET_USER_INFO", data);
-    },
-    removeUserInfo: ({ commit }) => {
-      localStorage.setItem("userInfo", "");
-      commit("SET_USER_INFO", {});
-    },
-    resetUserInfo: ({ dispatch }) => {
-      dispatch("removeToken");
-      dispatch("removeUserInfo");
-    },
-    toggleRefreshFailPopup: ({ commit }, value) => {
-      commit("SET_REFRESH_FAIL", value);
-    },
-    requestRefreshToken: ({ getters, dispatch, commit }, obj) => {
-      const { func, params } = obj;
-      axios({
-        url: drf.token.token(),
-        method: "post",
-        data: {
-          token: getters.getRefreshToken,
-        },
-      })
-        .then((response) => {
-          console.log("토큰 성공");
-          const data = response.data;
-          const token = {
-            accessToken: data.accessToken,
-            refreshToken: data.refreshToken,
-          };
-          dispatch("saveToken", token);
-          dispatch(func, params, { root: true });
-        })
-        .catch(() => {
-          dispatch("resetUserInfo");
-          clickHome();
-          commit("SET_REFRESH_FAIL", true);
-        });
-    },
-    changeProfileImage: ({ getters, dispatch }, params) => {
-      const { imageFile, profileImage } = params;
-      axios({
-        url: drf.accounts.profile(),
-        method: "post",
-        headers: {
-          Authorization: getters.getAccessToken,
-          "Content-Type": "multipart/form-data",
-        },
-        data: {
-          email: getters.getUserInfoId,
-          profileLink: imageFile,
-        },
-      })
-        .then((response) => {
-          profileImage.image = response.data.url;
-        })
-        .catch((error) => {
-          if (error.response.status !== 401) {
-            // 실패 팝업
-            dispatch("account/toggleUserInfoChangeError", true, { root: true });
-          } else {
-            // refreshToken 재발급
-            const obj = {
-              func: "personalInfo/changeProfileImage",
-              params,
-            };
-            dispatch("requestRefreshToken", obj);
-          }
-        });
-    },
-    submitChangeUserInfoForm: ({ dispatch, getters }, params) => {
-      const { name, nickname, profileLink, introduce } = params;
-      axios({
-        url: drf.accounts.profile(),
-        method: "put",
-        headers: {
-          Authorization: getters.getAccessToken,
-          "Content-Type": "multipart/form-data",
-        },
-        data: {
-          email: getters.getUserInfoId,
-          name,
-          nickname,
-          profileLink,
-          introduce,
-        },
-      })
-        .then((response) => {
-          // localStorage에 저장,vuex에 저장
-          dispatch("saveUserInfo", response.data);
+	actions: {
+		saveToken: ({ commit }, { accessToken, refreshToken }) => {
+			localStorage.setItem("accessToken", accessToken);
+			localStorage.setItem("refreshToken", refreshToken);
+			commit("SET_ACCESS_TOKEN", accessToken);
+			commit("SET_REFRESH_TOKEN", refreshToken);
+		},
+		removeToken: ({ commit }) => {
+			localStorage.setItem("accessToken", "");
+			localStorage.setItem("refreshToken", "");
+			commit("SET_ACCESS_TOKEN", "");
+			commit("SET_REFRESH_TOKEN", "");
+		},
+		saveUserInfo: ({ commit }, data: object) => {
+			const jsonUserInfo = JSON.stringify(data);
+			localStorage.setItem("userInfo", jsonUserInfo);
+			commit("SET_USER_INFO", data);
+		},
+		removeUserInfo: ({ commit }) => {
+			localStorage.setItem("userInfo", "");
+			commit("SET_USER_INFO", {});
+		},
+		resetUserInfo: ({ dispatch }) => {
+			dispatch("removeToken");
+			dispatch("removeUserInfo");
+		},
+		toggleRefreshFailPopup: ({ commit }, value) => {
+			commit("SET_REFRESH_FAIL", value);
+		},
+		requestRefreshToken: ({ getters, dispatch, commit }, obj) => {
+			const { func, params } = obj;
+			axios({
+				url: api.token.token(),
+				method: "post",
+				data: {
+					token: getters.getRefreshToken,
+				},
+			})
+				.then((response) => {
+					console.log("토큰 성공");
+					const data = response.data;
+					const token = {
+						accessToken: data.accessToken,
+						refreshToken: data.refreshToken,
+					};
+					dispatch("saveToken", token);
+					dispatch(func, params, { root: true });
+				})
+				.catch(() => {
+					dispatch("resetUserInfo");
+					clickHome();
+					commit("SET_REFRESH_FAIL", true);
+				});
+		},
+		changeProfileImage: ({ getters, dispatch }, params) => {
+			const { imageFile, profileImage } = params;
+			axios({
+				url: api.accounts.profile(),
+				method: "post",
+				headers: {
+					Authorization: getters.getAccessToken,
+					"Content-Type": "multipart/form-data",
+				},
+				data: {
+					email: getters.getUserInfoId,
+					profileLink: imageFile,
+				},
+			})
+				.then((response) => {
+					profileImage.image = response.data.url;
+				})
+				.catch((error) => {
+					if (error.response.status !== 401) {
+						// 실패 팝업
+						dispatch("account/toggleUserInfoChangeError", true, { root: true });
+					} else {
+						// refreshToken 재발급
+						const obj = {
+							func: "personalInfo/changeProfileImage",
+							params,
+						};
+						dispatch("requestRefreshToken", obj);
+					}
+				});
+		},
+		submitChangeUserInfoForm: ({ dispatch, getters }, params) => {
+			const { name, nickname, profileLink, introduce } = params;
+			axios({
+				url: api.accounts.profile(),
+				method: "put",
+				headers: {
+					Authorization: getters.getAccessToken,
+					"Content-Type": "multipart/form-data",
+				},
+				data: {
+					email: getters.getUserInfoId,
+					name,
+					nickname,
+					profileLink,
+					introduce,
+				},
+			})
+				.then((response) => {
+					// localStorage에 저장,vuex에 저장
+					dispatch("saveUserInfo", response.data);
 
-          //성공 팝업
-          dispatch("account/toggleUserInfoChangeSuccess", true, { root: true });
-        })
-        .catch((error) => {
-          if (error.response.status !== 401) {
-            // 실패 팝업
-            dispatch("account/toggleUserInfoChangeError", true, { root: true });
-          } else {
-            // refreshToken 재발급
-            const obj = {
-              func: "personalInfo/submitChangeUserInfoForm",
-              params,
-            };
-            dispatch("requestRefreshToken", obj);
-          }
-        });
-    },
-  },
+					//성공 팝업
+					dispatch("account/toggleUserInfoChangeSuccess", true, { root: true });
+				})
+				.catch((error) => {
+					if (error.response.status !== 401) {
+						// 실패 팝업
+						dispatch("account/toggleUserInfoChangeError", true, { root: true });
+					} else {
+						// refreshToken 재발급
+						const obj = {
+							func: "personalInfo/submitChangeUserInfoForm",
+							params,
+						};
+						dispatch("requestRefreshToken", obj);
+					}
+				});
+		},
+	},
 };

a33d2be2b882fa40712ef1703fd8c9ef61965e5c
SSAFY <seona0667@naver.com>
2022-07-29 18:00:15 +0900

    [SearchKeywordDto] Feat. 칵테일 사전: BE /  [검색 결과 API 기능 구현] 칵테일 정보(칵테일 이름, 사진, 재료(기주, 첨가물 3가지)와 유저 정보를 전달하는 각각의 DTO를 만든다.

---
+++ backend/src/main/java/com/jourgeois/backend/api/dto/SearchKeywordDto.java
@@ -0,0 +1,14 @@
+package com.jourgeois.backend.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class SearchKeywordDto {
+    private String name;
+    private String nameKr;
+    private String type;
+}

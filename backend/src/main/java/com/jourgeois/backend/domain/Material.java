dcfff69841965a387b4eaadaa2f908cc30e789d0
Seung Jun <jsznawa@gmail.com>
2022-07-29 13:11:52 +0900

    [S07P12B102-182] Feat. 칵테일 사전 기능: BE / [칵테일 상세 정보 API 기능 구현] 칵테일 Domain을 구현한다.

---
+++ backend/src/main/java/com/jourgeois/backend/domain/Material.java
@@ -0,0 +1,31 @@
+package com.jourgeois.backend.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Material {
+    @Id @GeneratedValue
+    @Column(name = "m_id")
+    private Long id;
+    @Column(name = "m_name")
+    private String name;
+    @Column(name = "m_img")
+    private String img;
+
+    @OneToMany(mappedBy = "material")
+    private List<CocktailToMaterial> cocktailToMaterials = new ArrayList<>();
+
+    public Material(Long id, String name, String img) {
+        this.id = id;
+        this.name = name;
+        this.img = img;
+    }
+}

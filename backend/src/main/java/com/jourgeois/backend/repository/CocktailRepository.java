c616f5085030bd40631898ba324625a6c2ecba8e
SSAFY <seona0667@naver.com>
2022-07-29 18:13:50 +0900

    [S07P12B102-173,176] Feat. 칵테일 사전: BE /  [검색 결과 API 기능 구현] 검색 키워드에 맞는 칵테일 정보와 유저 정보를 반환하는 Repository / Service 구현

--- backend/src/main/java/com/jourgeois/backend/repository/CocktailRepository.java
+++ backend/src/main/java/com/jourgeois/backend/repository/CocktailRepository.java
@@ -1,2 +1,11 @@
-package com.jourgeois.backend.repository;public interface CocktailRepository {
+package com.jourgeois.backend.repository;
+
+import com.jourgeois.backend.domain.Cocktail;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CocktailRepository extends JpaRepository<Cocktail, String> {
+    List<Cocktail> findByNameContainingOrderByName(String name, Pageable pageable);
 }

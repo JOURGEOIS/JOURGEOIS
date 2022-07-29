4103f578aae8cc7b970696a966a141b1153f090c
SSAFY <seona0667@naver.com>
2022-07-29 18:28:25 +0900

    [20220729] Fix. 설정해결

--- backend/src/main/java/com/jourgeois/backend/controller/CocktailController.java
+++ backend/src/main/java/com/jourgeois/backend/controller/CocktailController.java
@@ -1,14 +1,7 @@
 package com.jourgeois.backend.controller;
 
-import com.jourgeois.backend.service.CocktailService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/cocktail")
--- backend/src/main/java/com/jourgeois/backend/service/CocktailService.java
+++ backend/src/main/java/com/jourgeois/backend/service/CocktailService.java
@@ -2,8 +2,6 @@ package com.jourgeois.backend.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class CocktailService {
 

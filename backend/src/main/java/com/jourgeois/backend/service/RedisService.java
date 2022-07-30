8b9a68fe6fe218bbbdff53ac59bec5e500cd6508
Psalmist Kim <dss02094@gmail.com>
2022-07-30 20:54:00 +0900

    [S07P12B102] 칵테일 사전 - 검색 및 필터  : BE / [유저 최근 검색어 5개 정보 API] Contoller는 요청에 따라 유저 이메일(input)을 받아서 유저의 최근 검색어 5개(output)를 전달한다.

--- backend/src/main/java/com/jourgeois/backend/service/RedisService.java
+++ backend/src/main/java/com/jourgeois/backend/service/RedisService.java
@@ -1,13 +1,17 @@
 package com.jourgeois.backend.service;
 
+import com.jourgeois.backend.api.dto.SearchHistoryDto;
+import com.jourgeois.backend.api.dto.SearchTrendDto;
 import com.jourgeois.backend.domain.auth.EmailToken;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Synchronize;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -15,7 +19,11 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
 
     private static final String EMAIL_TOKEN = "emailToken:";
+    private static final String RECENT_KEYWORD = "recentKeyword:";
+    private static final String HOT_KEYWORD = "hotKeyword:";
     public final RedisTemplate<String, Object> redisTemplate;
+
+    public final RedisTemplate<String, String> redisStringTemplate;
 
     @Transactional
     public boolean setToken(EmailToken emailToken) throws Exception {
@@ -37,5 +45,37 @@ public class RedisService {
     public boolean deleteToken(String email) throws Exception {
         String key = EMAIL_TOKEN + email;
         return  redisTemplate.delete(key);
+    }
+
+    public boolean setRecentKeyword(String email, String keyword) throws Exception {
+        String key = RECENT_KEYWORD + email;
+        redisTemplate.opsForZSet().add(key, keyword, System.nanoTime());
+        System.out.println(key + "성공");
+        return true;
+    }
+
+    @Transactional
+    public Object[] getRecentKeyword(String email) throws Exception {
+        String key = RECENT_KEYWORD + email;
+
+        Set<ZSetOperations.TypedTuple<String>> res = redisStringTemplate.opsForZSet().reverseRangeWithScores(key, 0, 4);
+        redisTemplate.opsForZSet().removeRange(key, -5, -5);
+
+        System.out.println(key + "성공");
+        return res.toArray();
+    }
+
+    public boolean setHotKeywords(String timepoint, SearchTrendDto searchTrendDto) {
+        String key = HOT_KEYWORD + timepoint;
+        redisTemplate.opsForValue().set(key, searchTrendDto);
+        System.out.println("저장 성공");
+        return true;
+    }
+    public SearchTrendDto getHotKeywords(String timepoint) throws Exception {
+        String key = HOT_KEYWORD + timepoint;
+        SearchTrendDto searchTrend = (SearchTrendDto) redisTemplate.opsForValue().get(key);
+        System.out.println("가져오는 것 까지는 오키이이이ㅣ");
+        System.out.println("조회 성공");
+        return searchTrend;
     }
 }

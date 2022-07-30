6d529de2e7e8bfa925fbf94484f2b229c874e1ab
고승효 <shko0524@naver.com>
2022-07-30 22:08:57 +0900

    [S07P12B102-150] Feat. 칵테일사전: 후기 탭 게시글 작성 form 구현

--- frontend/src/components/cocktails/TheCocktailDescReview.vue
+++ frontend/src/components/cocktails/TheCocktailDescReview.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>후기</h1>
+  <TheCocktailDescReviewForm></TheCocktailDescReviewForm>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheCocktailDescReviewForm from '@/components/cocktails/TheCocktailDescReviewForm.vue'
+</script>
 
 <style scoped lang="scss"></style>

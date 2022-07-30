3c81183e3bf23770250084c49a563da729421979
박승훈 <tmdgns1126@naver.com>
2022-07-30 18:01:37 +0900

    [S07P12B102-135] Feat. 칵테일 검색: 자동완성 목록 구현

--- frontend/src/components/cocktails/TheAutoComplete.vue
+++ frontend/src/components/cocktails/TheAutoComplete.vue
@@ -1,14 +1,44 @@
 <template>
   <section class="auto-complete-section">
-    <h1 class="title1">이걸 찾으시나요?</h1>
-    <article class="no-content" v-if="!autoCompleteSearchWords.length">
-      검색 결과가 없습니다.
-    </article>
-    <article class="auto-complete-list" v-if="autoCompleteSearchWords.length">
+    <!-- 칵테일 -->
+    <article
+      class="auto-complete-list"
+      v-if="autoCompleteSearchWords.cocktails.length"
+    >
+      <h1 class="title1">칵테일</h1>
       <the-auto-complete-item
-        v-for="(item, idx) in autoCompleteSearchWords"
+        v-for="(item, idx) in autoCompleteSearchWords.cocktails"
         :key="idx"
         :data="item"
+        @click="clickCocktail(item)"
+      ></the-auto-complete-item>
+    </article>
+
+    <!-- 재료 -->
+    <article
+      class="auto-complete-list"
+      v-if="autoCompleteSearchWords.ingredients.length"
+    >
+      <h1 class="title1">재료</h1>
+      <the-auto-complete-item
+        v-for="(item, idx) in autoCompleteSearchWords.ingredients"
+        :key="idx"
+        :data="item"
+        @click="clickIngredient(item)"
+      ></the-auto-complete-item>
+    </article>
+
+    <!-- 계정 -->
+    <article
+      class="auto-complete-list"
+      v-if="autoCompleteSearchWords.users.length"
+    >
+      <h1 class="title1">계정</h1>
+      <the-auto-complete-item
+        v-for="(item, idx) in autoCompleteSearchWords.users"
+        :key="idx"
+        :data="item"
+        @click="clickUser(item)"
       ></the-auto-complete-item>
     </article>
   </section>
@@ -17,7 +47,9 @@
 <script setup lang="ts">
 import TheAutoCompleteItem from "@/components/cocktails/TheAutoCompleteItem.vue";
 import { reactive, computed, watchEffect } from "vue";
+import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+const router = useRouter();
 const store = useStore();
 
 const searchInputValue = computed(() => {
@@ -26,7 +58,7 @@ const searchInputValue = computed(() => {
 
 // * 검색 자동완성 리스트
 const autoCompleteSearchWords = computed(() => {
-  return store.getters["cocktails/getAutoCompleteSearchWords"];
+  return store.getters["cocktailSearch/getAutoCompleteSearchWords"];
 });
 
 // * 검색 자동완성 리스트 추가
@@ -46,6 +78,29 @@ watchEffect(() => {
     setAutoCompleteSearchWords(searchInputValue.value);
   }, 200);
 });
+
+// * 자동완성 클릭
+interface autoCompleteWord {
+  id: number;
+  name: string;
+  nameKr: string;
+  type: string;
+}
+
+const clickCocktail = (item: autoCompleteWord) => {
+  alert(item.nameKr);
+  // router.push({ name: "TheHomeView", params: {  } });
+};
+
+const clickIngredient = (item: autoCompleteWord) => {
+  alert(item.nameKr);
+  // router.push({ name: "TheHomeView", params: { } });
+};
+
+const clickUser = (item: autoCompleteWord) => {
+  alert(item.nameKr);
+  // router.push({ name: "TheProfileView", params: { userId: item.id } });
+};
 </script>
 <style scoped lang="scss">
 .title1 {
@@ -55,5 +110,9 @@ watchEffect(() => {
 .no-content {
   @include flex-xy(center);
   @include font-size-sub(13px);
+}
+
+.auto-complete-list {
+  margin-bottom: 40px;
 }
 </style>

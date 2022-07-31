8a64ab737fe9958736f87ad8db4f30e7d27e88ab
Psalmist Kim <dss02094@gmail.com>
2022-07-31 15:58:13 +0900

    [S07P12B102-231] [주간 인기 검색어 API 기능 구현] 한 주 인기 검색어 5개를 보여준다.

--- backend/src/main/java/com/jourgeois/backend/repository/CocktailRepository.java
+++ backend/src/main/java/com/jourgeois/backend/repository/CocktailRepository.java
@@ -18,7 +18,7 @@ public interface CocktailRepository extends JpaRepository<Cocktail, String> {
     @Query("SELECT c.id, c.name, c.nameKR, c.alcohol, c.cupId, c.tag, c.baseLiquor, c.category, c.recipe FROM Cocktail c WHERE c.id = :id")
     Optional<String> findCocktailById(@Param("id") Long id);
 
-    @Query("SELECT cp.nameKR FROM Cocktail c JOIN Cup cp ON c.cupId = cp.id WHERE c.id = :id")
+    @Query("SELECT cp.nameKR FROM Cocktail as c JOIN Cup as cp ON c.cupId = cp.id WHERE c.id = :id")
     Optional<String> findCocktailCupById(@Param("id") Long id);
     @Query("SELECT m.nameKR, m.img FROM Cocktail c JOIN CocktailToMaterial cm ON c.id = cm.cocktail.id JOIN Material m ON cm.material.id = m.id WHERE c.id = :id")
     Optional<ArrayList<String>> findAllMaterialsByCocktailId(@Param("id") Long id);

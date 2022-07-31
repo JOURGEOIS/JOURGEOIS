0cea27d60858102fd49f4167a509b515b3b5e665
Seung Jun <jsznawa@gmail.com>
2022-07-31 19:55:43 +0900

    [S07P12B102-183] fix/ BE / [칵테일 상세 정보 API 기능 구현] 칵테일 정보를 전달하는 Repository, Service 로직을 구현한다.

--- backend/src/main/java/com/jourgeois/backend/api/dto/CocktailDTO.java
+++ backend/src/main/java/com/jourgeois/backend/api/dto/CocktailDTO.java
@@ -1,23 +1,15 @@
 package com.jourgeois.backend.api.dto;
 
-import lombok.*;
 
-import java.util.ArrayList;
-
-@Getter @Setter
-@Builder
-@ToString
-public class CocktailDTO {
-
-    private String id;
-    private String name;
-    private String nameKR;
-    private String alcohol;
-    private String cupName;
-    private String tag;
-    private String baseLiquor;
-    private String category;
-    private String recipe;
-    private String img;
-    private ArrayList<String> materials;
+public interface CocktailDTO {
+    String getId();
+    String getName();
+    String getNameKR();
+    Double getAlcohol();
+    Integer getCupId();
+    String getTag();
+    String getBaseLiquor();
+    String getCategory();
+    String getRecipe();
+    String getImg();
 }

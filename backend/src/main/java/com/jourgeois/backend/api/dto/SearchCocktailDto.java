8bb52ac616e9a0dd3294d7dbeccdfc775c0057cb
SSAFY <seona0667@naver.com>
2022-07-31 13:58:58 +0900

    [S07P12B102-230] Fext. 칵테일 사전: [검색 결과 API 기능 구현] 이미지 정보 전달

--- backend/src/main/java/com/jourgeois/backend/api/dto/SearchCocktailDto.java
+++ backend/src/main/java/com/jourgeois/backend/api/dto/SearchCocktailDto.java
@@ -1,17 +1,14 @@
 package com.jourgeois.backend.api.dto;
 
-import com.jourgeois.backend.domain.Cocktail;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Builder @Getter @Setter
 public class SearchCocktailDto {
     // uid, 이름, 도수, 이미지, 기주
     private Long id;
+    private String img;
     private String name;
     private double alcohol;
     private String baseLiquor;

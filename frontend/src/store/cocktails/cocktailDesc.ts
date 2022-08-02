e059ba49aacadc211166bbcc929d2410988b769f
고승효 <shko0524@naver.com>
2022-08-02 16:44:53 +0900

    [S07P12B102-150] Feat. 칵테일사전: 욕설 필터링 구현

--- frontend/src/store/cocktails/cocktailDesc.ts
+++ frontend/src/store/cocktails/cocktailDesc.ts
@@ -1,15 +1,16 @@
-import axios from "axios";
-import api from "../../api/api";
-import { Module } from "vuex";
-import { RootState } from "../index";
+import axios from 'axios'
+import api from '../../api/api'
+import { Module } from 'vuex'
+import { RootState } from '../index'
 
 export interface cocktailData {
-  [key: string]: string | string[];
+  [key: string]: string | string[]
 }
 
 export interface CocktailDescState {
-  currentTab: number;
-  currentCocktailData: cocktailData;
+  currentTab: number
+  currentCocktailData: cocktailData
+  failPopupStatus: boolean
 }
 
 export const cocktailDesc: Module<CocktailDescState, RootState> = {
@@ -18,47 +19,55 @@ export const cocktailDesc: Module<CocktailDescState, RootState> = {
   state: {
     currentTab: 0,
     currentCocktailData: {},
+    failPopupStatus: false,
   },
   getters: {
     getCurrentTab: (state) => {
-      return state.currentTab;
+      return state.currentTab
     },
     getCurrentCocktailData: (state) => {
-      return state.currentCocktailData;
+      return state.currentCocktailData
     },
     getCurrentCocktailDataIngredients: (state) => {
-      return state.currentCocktailData.materials;
+      return state.currentCocktailData.materials
     },
-
+    getFailPopupStatus: (state) => {
+      return state.failPopupStatus
+    },
   },
   mutations: {
     SET_CURRENT_TAB: (state, value: number) => {
-      state.currentTab = value;
+      state.currentTab = value
     },
     SET_CURRENT_COCKTAIL_DATA: (state, value: cocktailData) => {
-      state.currentCocktailData = value;
+      state.currentCocktailData = value
     },
-
+    SET_FAIL: (state, value) => {
+      state.failPopupStatus = value
+    },
   },
   actions: {
     changeCurrentTab: ({ commit }, value: number) => {
-      commit("SET_CURRENT_TAB", value);
+      commit('SET_CURRENT_TAB', value)
     },
     getCocktailDb: ({ commit }, id: number) => {
       axios({
         url: api.cocktail.getCocktailData(),
-        method: "get",
+        method: 'get',
         params: {
           id,
         },
       })
         .then((response) => {
-          console.log(response.data);
-          commit("SET_CURRENT_COCKTAIL_DATA", response.data);
+          console.log(response.data)
+          commit('SET_CURRENT_COCKTAIL_DATA', response.data)
         })
         .catch((error) => {
-          console.log(error.response);
-        });
+          console.log(error.response)
+        })
+    },
+    toggleFailPopup: ({ commit }, value: boolean) => {
+      commit('SET_FAIL', value)
     },
   },
-};
+}

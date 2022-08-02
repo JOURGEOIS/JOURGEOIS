bec6a17e29ed95a275a48f076a8480f6ca750a9a
Psalmist Kim <dss02094@gmail.com>
2022-08-02 19:05:34 +0900

    [S07P12B102-273] 피드 - 뉴스피드 : BE / [뉴스피드] 유저가 게시글을 작성할 수 있다.

--- backend/src/main/java/com/jourgeois/backend/config/RedisConfig.java
+++ backend/src/main/java/com/jourgeois/backend/config/RedisConfig.java
@@ -18,15 +18,15 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
-    @Value("${spring.redis.password}")
-    private String password;
+//    @Value("${spring.redis.password}")
+//    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
+//        redisStandaloneConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 

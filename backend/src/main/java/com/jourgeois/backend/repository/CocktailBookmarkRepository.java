1c52cc3e90ab90528caae1dc2ae379e2257a1b9e
Seona Song <seona0667@naver.com>
2022-08-04 12:11:53 +0900

    [S07P12B102-266] Fest. 칵테일 사전: [원본 칵테일 북마크] 해당 칵테일을 북마크한 사람들의 리스트 목록 API 구현

--- backend/src/main/java/com/jourgeois/backend/repository/CocktailBookmarkRepository.java
+++ backend/src/main/java/com/jourgeois/backend/repository/CocktailBookmarkRepository.java
@@ -4,12 +4,15 @@ import com.jourgeois.backend.domain.cocktail.Cocktail;
 import com.jourgeois.backend.domain.cocktail.CocktailBookmark;
 import com.jourgeois.backend.domain.cocktail.CocktailBookmarkId;
 import com.jourgeois.backend.domain.member.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CocktailBookmarkRepository extends JpaRepository<CocktailBookmark, CocktailBookmarkId> {
 //    Optional<CocktailBookmark> findByMemberAndCocktail(Member member, Cocktail cocktail);
 //    Long countByCocktail(Cocktail cocktail);
     Long countByCocktailId(Cocktail c_id);
+    List<CocktailBookmark> findByCocktailId(Cocktail c_id, Pageable pageable);
 }
--- backend/src/main/java/com/jourgeois/backend/repository/FollowRepository.java
+++ backend/src/main/java/com/jourgeois/backend/repository/FollowRepository.java
@@ -9,9 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, FollowPK> {
 
     @Query(value = "select member.uid as uid, member.nickname as nickname, member.profile_img as profileImg, (uid in (select to_user_id from follow where from_user_id = :uid)) as isFollowed from member where uid in (select from_user_id from follow where to_user_id = :uid)", nativeQuery = true)
     List<FollowerVO> getFollwerAll(@Param(value = "uid") Long uid, Pageable pageable);
+
+//    Optional<Follow> findByFromAndTo(FollowPK key);
 }

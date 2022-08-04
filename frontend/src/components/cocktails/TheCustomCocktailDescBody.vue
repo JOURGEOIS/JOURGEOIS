2973955ee43d627b9f752778c630ab0349459779
박승훈 <tmdgns1126@naver.com>
2022-08-05 01:24:02 +0900

    [S07P12B102-157] Feat. 커스텀칵테일: 게시물 작성 유저 섹션 컴포넌트 제작

---
+++ frontend/src/components/cocktails/TheCustomCocktailDescBody.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="the-custom-cocktail-desc-body">
+    <post-user-section
+      :userInfo="data.followerDTO"
+      :date="data.customCocktail.createTime"
+    ></post-user-section>
+  </section>
+</template>
+
+<script setup lang="ts">
+import PostUserSection from "@/components/basics/PostUserSection.vue";
+import { CustomCocktail } from "../../interface";
+
+const props = defineProps<{
+  data: CustomCocktail;
+}>();
+</script>
+
+<style scoped lang="scss">
+.the-custom-cocktail-desc-body {
+  width: 100%;
+}
+</style>

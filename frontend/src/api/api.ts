45dfa83ed181e0b4c06daa09965cecd433dc458e
박승훈 <tmdgns1126@naver.com>
2022-08-06 21:38:21 +0900

    [S07P12B102-154] Feat. 커스텀칵테일: 세부 페이지 구현

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -51,6 +51,6 @@ export default {
     comment: () => HOST + POST + "auth/review",
     toggleBookmark: () => HOST + POST + "auth/bookmark",
     likedUsers: () => HOST + POST + "auth/like/list",
-    listFeed: () => HOST + POST + "auth/feed"
+    listFeed: () => HOST + POST + "auth/feed",
   },
 };
--- frontend/src/views/TheCustomCocktailDescView.vue
+++ frontend/src/views/TheCustomCocktailDescView.vue
@@ -5,14 +5,12 @@
       커스텀 칵테일
     </header-basic>
     <!-- 제목, 사진, 제조법, 설명 등 커스텀 칵테일 내용 -->
-    <the-custom-cocktail-desc-body
-      :data="customCocktailInfo"
-    ></the-custom-cocktail-desc-body>
+    <the-custom-cocktail-desc-body></the-custom-cocktail-desc-body>
     <!-- 댓글 좋아요 공유 부분 -->
-    <like-comment-share>
+    <!-- <like-comment-share>
       <template #like>888</template>
       <template #comment>777</template>
-    </like-comment-share>
+    </like-comment-share> -->
     <!-- 댓글 부분 -->
     <comment-form :page-id="customCocktailId"></comment-form>
     <comment-list :page-id="customCocktailId"></comment-list>
@@ -28,48 +26,29 @@ import TheLikeCommentShare from "@/components/basics/LikeCommentShare.vue";
 import CommentForm from "@/components/basics/CommentForm.vue";
 import CommentList from "@/components/basics/CommentList.vue";
 import NavBar from "@/components/basics/NavBar.vue";
-import { reactive, computed, onBeforeMount } from "vue";
-import { useRoute } from "vue-router";
+import { reactive, computed, onMounted, onUnmounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
 import { CustomCocktail } from "../interface";
+const router = useRouter();
 const route = useRoute();
+const store = useStore();
 
-const customCocktailInfo: CustomCocktail = {
-  customCocktail: {
-    postId: 1,
-    imgLink:
-      "https://cphoto.asiae.co.kr/listimglink/1/2020051809523742212_1589763157.jpg",
-    description:
-      "\“낮져밤이 마가리타\”빨간색과 파란색이 음양의 조화를 이룹니다. 완전 이쁘죠. 레몬까지 더해서 빨노파 붉은색 푸른색 그 사이 한 조각 레몬이 샤샤샤 아주 조화롭습니다. 뜨거운 낮과 차가운 밤을 표현해보았습니다.",
-    createTime: [2022, 8, 3, 22, 15, 12, 83204000],
-    lastUpdateTime: [2022, 8, 4, 9, 15, 12, 83204000],
-    isUpdated: 1,
-    like: 382,
-    title: "마!가린",
-    baseCocktail: 17216,
-    baseCocktailName: "토미스 마가리타",
-    ingredients: ["데킬라", "마가린", "치즈", "레몬"],
-    recipe:
-      "부어요. <> 막 부어요. <> 몰라 그냥 부어요. <> 냅다 부어요 그냥. <> 섞어요 엄청 섞어요",
-  },
-  followerDTO: {
-    uid: 2,
-    nickname: "우영우김밥하나요",
-    introduce: "꿀맛탱",
-    profileImg:
-      "https://t1.daumcdn.net/news/202206/14/SPORTSSEOUL/20220614092528280afnr.jpg",
-    isFollowed: 1,
-  },
-};
-
-// 게시글 id
-const customCocktailId = computed(() => {
-  return Number(route.params.customCocktailId);
+const originalCocktailId = Number(route.params.cocktailId);
+const customCocktailId = Number(route.params.customCocktailId);
+onMounted(() => {
+  store.dispatch("customCocktailInfo/setCustomCocktailDetail", {
+    customCocktailId,
+  });
 });
 
-console.log(customCocktailId);
+onUnmounted(() => {
+  store.dispatch("customCocktailInfo/removeCustomCocktailDetail");
+});
 
-// 게시글 id를 vuex에 저장한다.
+const customCocktailInfo = computed(() => {
+  return store.getters["customCocktailInfo/getCustomCocktailDetail"];
+});
 </script>
 
 <style scoped lang="scss">

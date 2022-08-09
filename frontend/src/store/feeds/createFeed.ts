6a4dddc22a9e6a47f60d8c95670e96a27416571a
JK <worudp12@gmail.com>
2022-08-10 03:57:35 +0900

    [S07P12B102-332] Feat. 기본컴포넌트: 이미지 유효성 검사

--- frontend/src/store/feeds/createFeed.ts
+++ frontend/src/store/feeds/createFeed.ts
@@ -1,21 +1,20 @@
-import { Module } from 'vuex'
-import { RootState } from '../index'
-import axios from 'axios'
-import api from '../../api/api'
-import { checkBadWord } from '../../functions/checkText'
-import router from '../../router'
+import { Module } from "vuex";
+import { RootState } from "../index";
+import axios from "axios";
+import api from "../../api/api";
+import { checkBadWord } from "../../functions/checkText";
+import router from "../../router";
 
 export interface CreateFeedState {
   // * (+) 팝업 모달
-  createFeedModalStatus: boolean
-  createFeedModalClass: string
+  createFeedModalStatus: boolean;
+  createFeedModalClass: string;
   // 팝업 알림
-  alertStatus: boolean
+  alertStatus: boolean;
   // 오류 메시지
-  errorMessage: string
+  errorMessage: string;
   // 성공 메시지
-  successMessage: string
-
+  successMessage: string;
 }
 
 export const createFeed: Module<CreateFeedState, RootState> = {
@@ -24,14 +23,13 @@ export const createFeed: Module<CreateFeedState, RootState> = {
   state: {
     // * (+) 팝업 모달
     createFeedModalStatus: false,
-    createFeedModalClass: '',
+    createFeedModalClass: "",
     // 에러 팝업 상태
     alertStatus: false,
     // 에러 메시지
-    errorMessage: '',
+    errorMessage: "",
     // 성공 메시지
-    successMessage: '',
-
+    successMessage: "",
   },
 
   getters: {
@@ -49,22 +47,22 @@ export const createFeed: Module<CreateFeedState, RootState> = {
   mutations: {
     // * (+) 팝업 모달
     SET_CREATE_FEED_MODAL_STATUS: (state, value: boolean) => {
-      state.createFeedModalStatus = value
+      state.createFeedModalStatus = value;
     },
     SET_CREATE_FEED_MODAL_CLASS: (state, value: string) => {
-      state.createFeedModalClass = value
+      state.createFeedModalClass = value;
     },
     // 에러 팝업
     SET_ALERT_STATUS: (state, value: boolean) => {
-      state.alertStatus = value
+      state.alertStatus = value;
     },
     // 에러 메시지
     SET_ERROR_MESSAGE: (state, value: string) => {
-      state.errorMessage = value
+      state.errorMessage = value;
     },
     // 성공 메시지
     SET_SUCCESS_MESSAGE: (state, value: string) => {
-      state.successMessage = value
+      state.successMessage = value;
     },
   },
 
@@ -78,60 +76,60 @@ export const createFeed: Module<CreateFeedState, RootState> = {
 
     // 알럿 팝업
     changeAlertStatus: ({ commit }, value: boolean) => {
-      commit('SET_ALERT_STATUS', value)
+      commit("SET_ALERT_STATUS", value);
     },
 
     //============== 제출
     // 일반 게시글 제출 전 유효성 검사 (빈 필드가 있는지 확인한다.)
     submitCommunityForm: ({ commit, dispatch }, data) => {
-      const { description, img } = data
+      const { description, img } = data;
 
       // 빈필드 확인 (빈 필드가 있으면 false)
-      const imgRequired = img instanceof File
-      const descriptionRequired = !!description
+      const imgRequired = img instanceof File;
+      const descriptionRequired = !!description;
 
       // 전부 true면 유효성 검사 통과!
       if (imgRequired && descriptionRequired) {
         // 비속어 유효성 검사, 공백으로 이뤄진 내용 유효성 검사
-        dispatch('checkTextInput', data)
+        dispatch("checkTextInput", data);
       } else {
-        commit('SET_ERROR_MESSAGE', '이미지와 내용은 필수 입력입니다')
-        commit('SET_ALERT_STATUS', true)
+        commit("SET_ERROR_MESSAGE", "이미지와 내용은 필수 입력입니다");
+        commit("SET_ALERT_STATUS", true);
       }
     },
 
     checkTextInput: ({ dispatch, commit }, data) => {
-      const { description } = data
+      const { description } = data;
       // 내용 중 빈 문자만 제출했는지 확인 (빈문자열이 있으면 false)
-      const descriptionNull = description.trim().length
-      console.log('type: ', descriptionNull)
+      const descriptionNull = description.trim().length;
+      console.log("type: ", descriptionNull);
       if (descriptionNull === 0) {
-        commit('SET_ERROR_MESSAGE', '내용에는 빈 값을 입력할 수 없습니다')
-        commit('SET_ALERT_STATUS', true)
-        return
+        commit("SET_ERROR_MESSAGE", "내용에는 빈 값을 입력할 수 없습니다");
+        commit("SET_ALERT_STATUS", true);
+        return;
       }
       // 비속어 확인 (비속어가 있으면 true)
-      const descriptionCheck = checkBadWord(description)
+      const descriptionCheck = checkBadWord(description);
 
       if (descriptionCheck) {
-        commit('SET_ERROR_MESSAGE', '부적절한 언어가 포함되었습니다')
-        commit('SET_ALERT_STATUS', true)
-        return
+        commit("SET_ERROR_MESSAGE", "부적절한 언어가 포함되었습니다");
+        commit("SET_ALERT_STATUS", true);
+        return;
       }
       // 저장
-      dispatch('saveCommunity', data)
+      dispatch("saveCommunity", data);
     },
 
     // 저장(생성)
     saveCommunity: ({ commit, rootGetters, dispatch }, data) => {
-      const { description, img } = data
+      const { description, img } = data;
       // 저장하기
       axios({
         url: api.post.postCocktail(),
-        method: 'post',
+        method: "post",
         headers: {
-          Authorization: rootGetters['personalInfo/getAccessToken'],
-          'Content-Type': 'multipart/form-data',
+          Authorization: rootGetters["personalInfo/getAccessToken"],
+          "Content-Type": "multipart/form-data",
         },
         data: {
           img,
@@ -139,110 +137,116 @@ export const createFeed: Module<CreateFeedState, RootState> = {
         },
       })
         .then((res) => {
-          console.log(res)
+          console.log(res);
           // 상세 페이지로 이동
           router.replace({
-            name: 'TheCommunityDescView',
+            name: "TheCommunityDescView",
             params: { feedId: res.data },
-          })
+          });
 
           // 성공 알림
-          console.log(res)
-          commit('SET_SUCCESS_MESSAGE', '성공적으로 등록되었습니다')
-          commit('SET_ALERT_STATUS', true)
+          console.log(res);
+          commit("SET_SUCCESS_MESSAGE", "성공적으로 등록되었습니다");
+          commit("SET_ALERT_STATUS", true);
         })
         .catch((error) => {
-          console.log(error)
+          console.log(error);
           if (error.response.status !== 401) {
             // 실패 팝업
-            commit('SET_ERROR_MESSAGE', '잠시 후에 시도해주세요')
-            commit('SET_ALERT_STATUS', true)
+            commit("SET_ERROR_MESSAGE", "잠시 후에 시도해주세요");
+            commit("SET_ALERT_STATUS", true);
           } else {
             // refreshToken 재발급
             const obj = {
-              func: 'createFeed/saveCommunity',
+              func: "createFeed/saveCommunity",
               params: data,
-            }
-            dispatch('personalInfo/requestRefreshToken', obj, { root: true })
+            };
+            dispatch("personalInfo/requestRefreshToken", obj, { root: true });
           }
-        })
+        });
     },
     // ================== 일반게시글 수정
     // 일반게시글 수정 시 유효성 검사
     updateCommunityForm: ({ commit, dispatch }, data) => {
-      const { description, img, postId } = data
-      console.log('유효성검사 data: ', data)
+      const { description, img, postId } = data;
+      console.log("유효성검사 data: ", data);
 
       // 내용 중 빈 문자만 제출했는지 확인 (빈문자열이 있으면 false)
-      const descriptionNull = description.trim().length
-      console.log('type: ', descriptionNull)
+      const descriptionNull = description.trim().length;
+      console.log("type: ", descriptionNull);
       if (descriptionNull === 0) {
-        commit('SET_ERROR_MESSAGE', '내용에는 빈 값을 입력할 수 없습니다')
-        commit('SET_ALERT_STATUS', true)
-        return
+        commit("SET_ERROR_MESSAGE", "내용에는 빈 값을 입력할 수 없습니다");
+        commit("SET_ALERT_STATUS", true);
+        return;
       }
       // 비속어 확인 (비속어가 있으면 true)
-      const descriptionCheck = checkBadWord(description)
+      const descriptionCheck = checkBadWord(description);
 
       if (descriptionCheck) {
-        commit('SET_ERROR_MESSAGE', '부적절한 언어가 포함되었습니다')
-        commit('SET_ALERT_STATUS', true)
-        return
+        commit("SET_ERROR_MESSAGE", "부적절한 언어가 포함되었습니다");
+        commit("SET_ALERT_STATUS", true);
+        return;
       }
       // 저장
-      dispatch('updateSaveCommunity', data)
+      dispatch("updateSaveCommunity", data);
     },
 
     // 일반게시글 수정
-    updateSaveCommunity: (
-      { commit, dispatch, rootGetters },
-      params,
-    ) => {
-      const { description, img, postId } = params
-      console.log(params)
+    updateSaveCommunity: ({ commit, dispatch, rootGetters }, params) => {
+      const { description, img, postId } = params;
+
+      // 요청 보낼 데이터
+      const data: any = {
+        description,
+        postId,
+      };
+      if (img) {
+        data.img = img;
+      }
+
       axios({
         url: api.post.postCocktail(),
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          Authorization: rootGetters['personalInfo/getAccessToken'],
-          'Content-Type': 'multipart/form-data',
+          Authorization: rootGetters["personalInfo/getAccessToken"],
+          "Content-Type": "multipart/form-data",
         },
-        data: params,
+        data,
       })
         .then((res) => {
-          console.log(res.data)
+          console.log(res.data);
           router.replace({
-            name: 'TheCommunityDescView',
+            name: "TheCommunityDescView",
             params: { feedId: res.data },
-          })
+          });
 
           // 성공 알림
-          console.log(res)
-          commit('SET_SUCCESS_MESSAGE', '성공적으로 변경되었습니다')
-          commit('SET_ALERT_STATUS', true)
+          console.log(res);
+          commit("SET_SUCCESS_MESSAGE", "성공적으로 변경되었습니다");
+          commit("SET_ALERT_STATUS", true);
         })
         .catch((err) => {
-          console.log(err)
+          console.log(err);
           if (err.response.status !== 401) {
             // 실패 팝업
-            commit('SET_ERROR_MESSAGE', '잠시 후에 시도해주세요')
-            commit('SET_ALERT_STATUS', true)
+            commit("SET_ERROR_MESSAGE", "잠시 후에 시도해주세요");
+            commit("SET_ALERT_STATUS", true);
           } else {
             // refreshToken 재발급
             const obj = {
-              func: 'createFeed/updateSaveCommunity',
+              func: "createFeed/updateSaveCommunity",
               params,
-            }
-            dispatch('personalInfo/requestRefreshToken', obj, { root: true })
+            };
+            dispatch("personalInfo/requestRefreshToken", obj, { root: true });
           }
-        })
+        });
     },
     // * (+) 팝업 모달
     toggleCreateFeedModal: ({ commit }, value: boolean) => {
-      commit('SET_CREATE_FEED_MODAL_STATUS', value)
+      commit("SET_CREATE_FEED_MODAL_STATUS", value);
     },
     changeCreateFeedModalClass: ({ commit }, value: string) => {
-      commit('SET_CREATE_FEED_MODAL_CLASS', value)
+      commit("SET_CREATE_FEED_MODAL_CLASS", value);
     },
   },
-}
+};

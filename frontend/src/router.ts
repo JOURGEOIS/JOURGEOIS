a05f8d07c2d9e60507d0915685c145a3165bfbb1
고승효 <shko0524@naver.com>
2022-08-11 06:47:16 +0900

    [S07P12B102-409] Feat. 회원관리: 내 프로필 기본 컴포넌트 마크업-네비게이션가드추가

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,206 +1,206 @@
-import { createRouter, createWebHistory, useRouter } from "vue-router";
-import createStore from "../src/store/index";
-import { computed } from "vue";
+import { createRouter, createWebHistory, useRouter } from 'vue-router'
+import createStore from '../src/store/index'
+import { computed } from 'vue'
 
 // 스토어 정의
-const store = createStore;
+const store = createStore
 
 const routes: any[] = [
   // 홈 화면
   {
-    path: "/",
-    name: "TheHomeView",
-    component: () => import("@/views/TheHomeView.vue"),
+    path: '/',
+    name: 'TheHomeView',
+    component: () => import('@/views/TheHomeView.vue'),
   },
 
   // 회원가입 화면
   {
-    path: "/user/signup",
-    name: "TheSignupView",
-    component: () => import("@/views/TheSignupView.vue"),
+    path: '/user/signup',
+    name: 'TheSignupView',
+    component: () => import('@/views/TheSignupView.vue'),
   },
 
   // 로그인 화면
   {
-    path: "/user/login",
-    name: "TheLoginView",
-    component: () => import("@/views/TheLoginView.vue"),
+    path: '/user/login',
+    name: 'TheLoginView',
+    component: () => import('@/views/TheLoginView.vue'),
   },
 
   // 카카오 로그인
   {
-    path: "/user/login/kakaologin",
-    name: "Kakaologin",
-    component: () => import("@/views/KakaoLogin.vue")
+    path: '/user/login/kakaologin',
+    name: 'Kakaologin',
+    component: () => import('@/views/KakaoLogin.vue'),
   },
 
   // 로그아웃 화면
   {
-    path: "/user/sign-out",
-    name: "TheSignOutView",
-    component: () => import("@/views/TheSignOutView.vue"),
+    path: '/user/sign-out',
+    name: 'TheSignOutView',
+    component: () => import('@/views/TheSignOutView.vue'),
   },
 
   // 유저 정보 수정
   {
-    path: "/user/my-info",
-    name: "TheChangeUserView",
-    component: () => import("@/views/TheChangeUserView.vue"),
+    path: '/user/my-info',
+    name: 'TheChangeUserView',
+    component: () => import('@/views/TheChangeUserView.vue'),
   },
 
   // 비밀번호 찾기 화면
   {
-    path: "/user/help/password",
-    name: "TheForgotPwView",
-    component: () => import("@/views/TheForgotPwView.vue"),
+    path: '/user/help/password',
+    name: 'TheForgotPwView',
+    component: () => import('@/views/TheForgotPwView.vue'),
   },
 
   // 비밀번호 변경 화면
   {
-    path: "/user/my-info/password",
-    name: "TheChangePwView",
-    component: () => import("@/views/TheChangePwView.vue"),
+    path: '/user/my-info/password',
+    name: 'TheChangePwView',
+    component: () => import('@/views/TheChangePwView.vue'),
   },
 
   // 내 프로필 화면
   {
-    path: "/user/my-profile",
-    name: "TheMyProfileView",
-    component: () => import("@/views/TheMyProfileView.vue"),
+    path: '/user/my-profile',
+    name: 'TheMyProfileView',
+    component: () => import('@/views/TheMyProfileView.vue'),
   },
 
   // 칵테일 검색 페이지
   {
-    path: "/cocktail/search",
-    name: "TheCocktailSearchView",
-    component: () => import("@/views/TheCocktailSearchView.vue"),
+    path: '/cocktail/search',
+    name: 'TheCocktailSearchView',
+    component: () => import('@/views/TheCocktailSearchView.vue'),
   },
 
   // 칵테일 검색 결과(재료) 페이지
   {
-    path: "/cocktail/search/byingredients/:ingredientId",
-    name: "TheSearchCocktailByIngredientView",
-    component: () => import("@/views/TheSearchCocktailByIngredientView.vue"),
+    path: '/cocktail/search/byingredients/:ingredientId',
+    name: 'TheSearchCocktailByIngredientView',
+    component: () => import('@/views/TheSearchCocktailByIngredientView.vue'),
   },
 
   // 칵테일 검색 결과 페이지
   {
-    path: "/cocktail/search/:searchValue",
-    name: "TheSearchResultView",
-    component: () => import("@/views/TheSearchResultView.vue"),
+    path: '/cocktail/search/:searchValue',
+    name: 'TheSearchResultView',
+    component: () => import('@/views/TheSearchResultView.vue'),
   },
   // 칵테일 필터 검색 결과 페이지
   {
-    path: "/cocktail/search/filter",
-    name: "TheSearchFilterResultView",
-    component: () => import("@/views/TheSearchFilterResultView.vue"),
+    path: '/cocktail/search/filter',
+    name: 'TheSearchFilterResultView',
+    component: () => import('@/views/TheSearchFilterResultView.vue'),
   },
 
   // 칵테일 전체 리스트 페이지
   {
-    path: "/cocktail/whole",
-    name: "TheWholeCocktailView",
-    component: () => import("@/views/TheWholeCocktailView.vue"),
+    path: '/cocktail/whole',
+    name: 'TheWholeCocktailView',
+    component: () => import('@/views/TheWholeCocktailView.vue'),
   },
 
   // 필터 카테고리
   {
-    path: "/cocktail/search/filter/:category",
-    name: "TheCocktailFilterCategoryView",
-    component: () => import("@/views/TheCocktailFilterCategoryView.vue"),
+    path: '/cocktail/search/filter/:category',
+    name: 'TheCocktailFilterCategoryView',
+    component: () => import('@/views/TheCocktailFilterCategoryView.vue'),
   },
 
   // 칵테일 상세 페이지
   {
-    path: "/cocktail/:cocktailId",
-    name: "TheCocktailDescView",
-    component: () => import("@/views/TheCocktailDescView.vue"),
+    path: '/cocktail/:cocktailId',
+    name: 'TheCocktailDescView',
+    component: () => import('@/views/TheCocktailDescView.vue'),
   },
 
   // 칵테일 상세 페이지 북마크
   {
-    path: "/cocktail/:cocktailId/bookmark",
-    name: "TheCocktailDescBookmarkView",
-    component: () => import("@/views/TheCocktailDescBookmarkView.vue"),
+    path: '/cocktail/:cocktailId/bookmark',
+    name: 'TheCocktailDescBookmarkView',
+    component: () => import('@/views/TheCocktailDescBookmarkView.vue'),
   },
 
   // 커스텀 칵테일 제작 페이지
   {
-    path: "/cocktail/:cocktailId/custom/form",
-    name: "TheCustomCocktailFormView",
-    component: () => import("@/views/TheCustomCocktailFormView.vue"),
+    path: '/cocktail/:cocktailId/custom/form',
+    name: 'TheCustomCocktailFormView',
+    component: () => import('@/views/TheCustomCocktailFormView.vue'),
   },
 
   // 커스텀 칵테일 상세 페이지
   {
-    path: "/cocktail/:cocktailId/custom/:feedId",
-    name: "TheCustomCocktailDescView",
-    component: () => import("@/views/TheCustomCocktailDescView.vue"),
+    path: '/cocktail/:cocktailId/custom/:feedId',
+    name: 'TheCustomCocktailDescView',
+    component: () => import('@/views/TheCustomCocktailDescView.vue'),
   },
 
   // 커스텀 칵테일 수정
   {
-    path: "/cocktail/:cocktailId/custom/:feedId/update",
-    name: "TheCustomCocktailUpdateFormView",
-    component: () => import("@/views/TheCustomCocktailUpdateFormView.vue"),
+    path: '/cocktail/:cocktailId/custom/:feedId/update',
+    name: 'TheCustomCocktailUpdateFormView',
+    component: () => import('@/views/TheCustomCocktailUpdateFormView.vue'),
   },
   ,
   // 좋아요한 유저 리스트 페이지
   {
-    path: "/feeds/:feedId/liked",
-    name: "TheLikedUserListView",
-    component: () => import("@/views/TheLikedUserListView.vue"),
+    path: '/feeds/:feedId/liked',
+    name: 'TheLikedUserListView',
+    component: () => import('@/views/TheLikedUserListView.vue'),
   },
 
   // 뉴스피드 리스트
   {
-    path: "/feeds",
-    name: "TheNewsFeedView",
-    component: () => import("@/views/TheNewsFeedView.vue"),
+    path: '/feeds',
+    name: 'TheNewsFeedView',
+    component: () => import('@/views/TheNewsFeedView.vue'),
   },
 
   // 뉴스피드 상세 페이지 (커칵, 일반 )
   {
-    path: "/feeds/:feedId",
-    name: "TheCommunityDescView",
-    component: () => import("@/views/TheCommunityDescView.vue"),
+    path: '/feeds/:feedId',
+    name: 'TheCommunityDescView',
+    component: () => import('@/views/TheCommunityDescView.vue'),
   },
 
   // 뉴스피드 게시글 form
   {
-    path: "/feeds/form",
-    name: "TheCommunityFormView",
-    component: () => import("@/views/TheCommunityFormView.vue"),
+    path: '/feeds/form',
+    name: 'TheCommunityFormView',
+    component: () => import('@/views/TheCommunityFormView.vue'),
   },
 
   // 뉴스피드 게시글 form 수정
   {
-    path: "/feeds/:feedId/update",
-    name: "TheCommunityUpdateFormView",
-    component: () => import("@/views/TheCommunityUpdateFormView.vue"),
+    path: '/feeds/:feedId/update',
+    name: 'TheCommunityUpdateFormView',
+    component: () => import('@/views/TheCommunityUpdateFormView.vue'),
   },
 
   // 뉴스피드 상세 페이지 (커칵)
   {
-    path: "/feeds/:feedId/custom",
-    name: "TheSuperCustomCocktailDescView",
-    component: () => import("@/views/TheSuperCustomCocktailDescView.vue"),
+    path: '/feeds/:feedId/custom',
+    name: 'TheSuperCustomCocktailDescView',
+    component: () => import('@/views/TheSuperCustomCocktailDescView.vue'),
   },
 
   // 슈커칵 게시글 form
   {
-    path: "/feeds/custom/form",
-    name: "TheSuperCustomCocktailFormView",
-    component: () => import("@/views/TheSuperCustomCocktailFormView.vue"),
+    path: '/feeds/custom/form',
+    name: 'TheSuperCustomCocktailFormView',
+    component: () => import('@/views/TheSuperCustomCocktailFormView.vue'),
   },
 
   // 슈커칵 게시글 수정 form
   {
-    path: "/feeds/:feedId/custom/update",
-    name: "TheSuperCustomCocktailUpdateFormView",
-    component: () => import("@/views/TheSuperCustomCocktailUpdateFormView.vue"),
+    path: '/feeds/:feedId/custom/update',
+    name: 'TheSuperCustomCocktailUpdateFormView',
+    component: () => import('@/views/TheSuperCustomCocktailUpdateFormView.vue'),
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
@@ -208,53 +208,54 @@ const router = createRouter({
 
   // 스크롤 무조건 맨 위로 이동
   scrollBehavior(to, from, savedPosition) {
-    return { top: 0 };
+    return { top: 0 }
   },
-});
+})
 
 // Navigation Guard 설정
 router.beforeEach((to: any, from, next) => {
   // 로그인 여부 확인
-  const isLoggedIn = computed(() => store.getters["personalInfo/isLoggedIn"]);
+  const isLoggedIn = computed(() => store.getters['personalInfo/isLoggedIn'])
 
   // 로그인이 필요한 페이지
   const authPages = [
-    "TheSignOutView",
-    "TheChangeUserView",
-    "TheChangePwView",
-    "TheCocktailDescBookmarkView",
-    "TheCustomCocktailFormView",
-    "TheCustomCocktailDescView",
-    "TheCustomCocktailUpdateFormView",
-    "TheLikedUserListView",
-    "TheNewsFeedView",
-    "TheCommunityDescView",
-    "TheCommunityFormView",
-    "TheCommunityUpdateFormView",
-    "TheSuperCustomCocktailDescView",
-    "TheSuperCustomCocktailFormView",
-    "TheSuperCustomCocktailUpdateFormView",
-  ];
+    'TheSignOutView',
+    'TheChangeUserView',
+    'TheChangePwView',
+    'TheMyProfileView',
+    'TheCocktailDescBookmarkView',
+    'TheCustomCocktailFormView',
+    'TheCustomCocktailDescView',
+    'TheCustomCocktailUpdateFormView',
+    'TheLikedUserListView',
+    'TheNewsFeedView',
+    'TheCommunityDescView',
+    'TheCommunityFormView',
+    'TheCommunityUpdateFormView',
+    'TheSuperCustomCocktailDescView',
+    'TheSuperCustomCocktailFormView',
+    'TheSuperCustomCocktailUpdateFormView',
+  ]
 
   // 로그인이 되어있지 않을 때만 가능한 페이지
-  const notAuthPages = ["TheSignupView", "TheLoginView"];
+  const notAuthPages = ['TheSignupView', 'TheLoginView']
 
   // 1. 현재 이동하고자 하는 페이지가 로그인이 필요한지 확인
-  const isAuthRequired = authPages.includes(to.name);
-  const isNotAuthRequired = notAuthPages.includes(to.name);
+  const isAuthRequired = authPages.includes(to.name)
+  const isNotAuthRequired = notAuthPages.includes(to.name)
 
   // 2. 로그인이 필요한 페이지인데 로그인이 되어있지 않다면 로그인 페이지(/login)로 이동
   if (isAuthRequired && !isLoggedIn.value) {
-    next({ name: "TheLoginView" });
+    next({ name: 'TheLoginView' })
 
     // 3. 로그인이 되어있는데 /login, /signup 페이지로 이동한다면 메인 페이지(/)로 이동
   } else if (isNotAuthRequired && isLoggedIn.value) {
-    next({ name: "TheHomeView" });
+    next({ name: 'TheHomeView' })
 
     // 4.  원래 이동 페이지
   } else {
-    next();
+    next()
   }
-});
+})
 
-export default router;
+export default router

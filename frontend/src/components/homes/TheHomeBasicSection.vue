eec76c8061c59e37a3fa5a8e8a61a3454c15bbe4
박승훈 <tmdgns1126@naver.com>
2022-08-10 16:04:09 +0900

    [S07P12B102-403] 홈: 기본 섹션 컴포넌트 UI 마크업

--- frontend/src/components/homes/TheHomeBasicSection.vue
+++ frontend/src/components/homes/TheHomeBasicSection.vue
@@ -1,15 +1,123 @@
 <template>
   <div class="home-basic-section">
-    <slot name="title"></slot>
+    <article class="article-title">
+      <slot></slot>
+      <span
+        class="material-icons-outlined icon-info"
+        @click="toggleShowDescription"
+      >
+        info
+      </span>
+    </article>
+    <article class="article-description" v-if="isShowDescription">
+      {{ data.description }}
+    </article>
+    <article class="article-carosel">
+      <div class="carosel-item">
+        <div class="carosel-img"></div>
+        <div class="carosel-text">마!가리타라라라라라라라라라라라라라라라</div>
+      </div>
+      <div class="carosel-item">
+        <div class="carosel-img"></div>
+        <div class="carosel-text">마!가리타라라라라라라라라라</div>
+      </div>
+    </article>
+    <article class="article-more-button">
+      <div class="more-button" @click="clickMore">
+        {{ data.moreButtonText }}
+      </div>
+    </article>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+const store = useStore();
+const router = useRouter();
+
+const props = defineProps<{
+  data: {
+    description: string;
+    moreButtonText: string;
+    callCaroselFunc: string;
+    showMoreView: string;
+  };
+}>();
+
+const isShowDescription = ref(false);
+
+const toggleShowDescription = () => {
+  isShowDescription.value = !isShowDescription.value;
+};
+
+// 더보기 클릭
+const clickMore = () => {
+  router.push({ name: props.data.showMoreView });
+};
+</script>
 
 <style scoped lang="scss">
 .home-basic-section {
+  @include flex(column);
   width: 100%;
-  background-color: $white150;
-  border-radius: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: $white;
+  gap: 15px;
+  @include shadow-feed;
+}
+
+// title slot
+.article-title {
+  @include flex-xy(flex-start, flex-end);
+  @include font(15px);
+  gap: 5px;
+
+  .icon-info {
+    @include font-size-placeholder(19px);
+  }
+}
+
+.article-description {
+  margin-top: -10px;
+  @include font(12px, $fw-regular);
+  color: $sub-color;
+}
+
+.article-carosel {
+  @include flex;
+  gap: 5px;
+  .carosel-item {
+    @include flex(column);
+    width: 130px;
+    gap: 5px;
+    .carosel-img {
+      // width: 150px;
+      // height: 150px;
+      height: 130px;
+      background-color: $white200;
+      border-radius: 5px;
+    }
+    .carosel-text {
+      width: 100%;
+      @include font-size-sub(12px);
+    }
+  }
+}
+
+.article-more-button {
+  .more-button {
+    @include flex-center;
+    width: 100%;
+    padding: 5px;
+    border: 1px solid $unchecked-color;
+    border-radius: 5px;
+    @include font(12px, $fw-medium);
+    color: $primary-color;
+    @include shadow-feed;
+    @include for-click;
+  }
 }
 </style>
--- frontend/src/components/homes/TheHomeViewContainer.vue
+++ frontend/src/components/homes/TheHomeViewContainer.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="home-view-container top-view-no-margin">
-    <the-home-basic-section></the-home-basic-section>
-    <the-home-basic-section></the-home-basic-section>
+    <!-- 유저들의 NEW 커스텀 칵테일 -->
+    <the-home-basic-section :data="newCustomCocktailData">
+      <h1 class="title">
+        유저들의 <span class="important">NEW</span> 커스텀 칵테일
+      </h1>
+    </the-home-basic-section>
   </div>
 </template>
 
 <script setup lang="ts">
 import TheHomeBasicSection from "@/components/homes/TheHomeBasicSection.vue";
+
+const newCustomCocktailData = {
+  description: "유저들이 최근에 등록한 커스텀 칵테일",
+  moreButtonText: "전체 보기",
+  callCaroselFunc: "",
+  showMoreView: "",
+};
 </script>
 
 <style scoped lang="scss">
 .home-view-container {
   @include flex(column);
-  width: 100%;
+  width: calc(100% + 32px);
   margin-top: 8px;
   gap: 20px;
+  background-color: $white150;
+  padding: 20px 0;
 
   @media #{$tablet} {
     width: 60%;
@@ -22,6 +35,15 @@ import TheHomeBasicSection from "@/components/homes/TheHomeBasicSection.vue";
 
   @media #{$pc} {
     width: 50%;
+  }
+}
+
+.title {
+  @include font(15px, $fw-medium);
+
+  .important {
+    @include font(15px, $fw-bold);
+    color: $danger-color;
   }
 }
 </style>

699d3567940c4bf36ad4f514e3840ae65719fcf1
Psalmist Kim <dss02094@gmail.com>
2022-08-11 11:13:52 +0900

    [S07P12B102-438] Fix. 피드 - 뉴스피드 : S07P12B102S07P12B102-438 BE / [뉴스피드] 비공개 계정은 안보이도록 한다.

--- backend/src/main/java/com/jourgeois/backend/api/dto/notification/NotificationDTO.java
+++ backend/src/main/java/com/jourgeois/backend/api/dto/notification/NotificationDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NotificationDTO {
-    private String notiId;
+//    private String notiId;
 
     // FOLLOW, LIKE, COMMENT
     private NotificationType type;

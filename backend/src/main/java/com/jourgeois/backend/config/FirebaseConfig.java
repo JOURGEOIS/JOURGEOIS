028c7edb6cb4a638f339767565ea2f3383c5a999
Seung Jun <jsznawa@gmail.com>
2022-08-11 17:40:02 +0900

    [S07P12B102-436] Fix. 피드 - 알림 : BE / [알림] 알림 모두 읽음 버튼 클릭시 해당 유저의 모든 알림을 읽음 처리

--- backend/src/main/java/com/jourgeois/backend/config/FirebaseConfig.java
+++ backend/src/main/java/com/jourgeois/backend/config/FirebaseConfig.java
@@ -3,18 +3,10 @@ package com.jourgeois.backend.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-<<<<<<< HEAD
-=======
-
->>>>>>> 208e5155a8d33a2f82635775b47a986919add836
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-<<<<<<< HEAD
-=======
-
->>>>>>> 208e5155a8d33a2f82635775b47a986919add836
 
 @Configuration
 public class FirebaseConfig {
@@ -22,10 +14,6 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-<<<<<<< HEAD
-            // Use a service account
-=======
->>>>>>> 208e5155a8d33a2f82635775b47a986919add836
             GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource("./serviceAccountKey.json").getInputStream());
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(credentials)

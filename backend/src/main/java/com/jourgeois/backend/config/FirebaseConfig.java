e8c3c28c60b8ff8052f1c2f14b1a58767a175bdb
Seung Jun <jsznawa@gmail.com>
2022-08-11 17:17:34 +0900

    [S07P12B102-300] Fix. BE / [소셜 로그인] 네이버, 구글, 카카오로 로그인을 할 수 있다.

--- backend/src/main/java/com/jourgeois/backend/config/FirebaseConfig.java
+++ backend/src/main/java/com/jourgeois/backend/config/FirebaseConfig.java
@@ -1,20 +1,12 @@
 package com.jourgeois.backend.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
-import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class FirebaseConfig {
@@ -22,11 +14,8 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            Path filePath = Paths.get(File.separatorChar + "serviceAccountKey.json");
-            FileInputStream serviceAccount = new FileInputStream(filePath.toString());
-            // FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
             // Use a service account
-            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource("./serviceAccountKey.json").getInputStream());
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(credentials)
                     .build();
@@ -34,5 +23,6 @@ public class FirebaseConfig {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }

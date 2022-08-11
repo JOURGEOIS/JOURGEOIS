c3d663301ad11816f696d2af6c4215010a35ec3c
Seung Jun <jsznawa@gmail.com>
2022-08-11 17:14:38 +0900

    [S07P12B102-300] Fix. BE / [소셜 로그인] 네이버, 구글, 카카오로 로그인을 할 수 있다.

--- frontend/src/components/accounts/TheSocialLogin.vue
+++ frontend/src/components/accounts/TheSocialLogin.vue
@@ -1,7 +1,11 @@
 <template>
-  <hr />
+  <!-- <hr /> -->
   <div class="social-login">
     <p>or</p>
+    <GoogleLogin
+      :callback="callback"
+      :button-style="['google-login', 'long', 'small']"
+    />
     <button-basic
       @click="clickGoogle"
       :button-style="['google-login', 'long', 'small']"
@@ -42,9 +46,10 @@
 import axios from "axios";
 import api from "../../api/api";
 import ButtonBasic from "@/components/basics/ButtonBasic.vue";
-import { computed, reactive } from "vue";
+import { computed, onMounted, reactive } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+// import { CallbackTypes } from "vue3-google-login";
 const store = useStore();
 const router = useRouter();
 
@@ -53,14 +58,22 @@ const googleAPI = computed(
   () => store.getters["socialLogin/getGoogleLoginApi"]
 );
 
+// const callback: CallbackTypes.CredentialCallback = (response) => {
+//   console.log("Credential JWT string", response.credential);
+// };
+
 const clickGoogle = () => {
-  // console.log(googleAPI.value)
-  // store.dispatch('socialLogin/getGoogleLoginApi', googleAPI)
-  window.open("http://jourgeois.com/api/member/login/google", "_blank");
+  const google_authorize_url = "ttps://accounts.google.com/o/oauth2/v2/auth";
+  const google_client_id =
+    "217608233279-k2op58rvkdtbbteakk7ag0tp5ia54mij.apps.googleusercontent.com";
+  const google_redirect_url =
+    "https://jourgeois.com/api/member/login/google/redirect";
+  window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${google_client_id}&redirect_uri=${google_redirect_url}&response_type=code&scope=profile%20email%20openid`;
 };
+
 const kakaoLogin = () => {
   window.Kakao.Auth.login({
-    scope: "profile, account_email",
+    scope: "profile_nickname, profile_image, account_email",
     success: getKakaoAccount,
   });
 };
@@ -71,6 +84,7 @@ const getKakaoAccount = () => {
       const kakao_account = res.kakao_account;
       const nickname = kakao_account.profile.nickname;
       const email = kakao_account.email;
+      console.log(kakao_account);
       console.log("nickname", nickname);
       console.log("email", email);
 
@@ -78,10 +92,11 @@ const getKakaoAccount = () => {
       alert("로그인 성공!");
     },
     fail: (error: any) => {
-      console.error(error);
+      console.log(error);
     },
   });
 };
+
 // const clickKakao = () => {
 //   // axios({
 //   //   url: api.accounts.kakaoLogin(),
@@ -94,23 +109,46 @@ const getKakaoAccount = () => {
 //   //   .catch((err) => {
 //   //     console.error(err.response)
 //   //   })
-//   window.open("https://kauth.kakao.com/oauth/authorize?client_id=f1c36f65322c75f1f28caf1560a306d1&redirect_uri=http://jourgeois.com/api/member/login/kakao/redirect&response_type=code")
+//   window.open("https://kauth.kakao.com/oauth/authorize?client_id=0c777eb20471ff56b13960d5e8534d5e&redirect_uri=https://jourgeois.com/api/member/login/kakao/redirect&response_type=code")
 // }
 
 const clickNaver = () => {
-  window.open("http://jourgeois.com/api/member/login/naver", "_blank");
-  // axios({
-  //   url: api.accounts.naverLogin(),
-  //   method: 'GET',
-  // })
-  //   .then((res) => {
-  //     const data = res.data
-  //     console.log(data)
-  //   })
-  //   .catch((err) => {
-  //     console.error(err.response)
-  //   })
+  window.location.href = `
+https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=D0B_Xn9oGT7I_66h9Z4U&redirect_uri=https://jourgeois.com/api/member/login/naver/redirect&state=78577e6c-3f10-4146-910c-3fd4e684dfb5`;
+  // const naverLogin = new naver.LoginWithNaverId({
+  //   clientId: "D0B_Xn9oGT7I_66h9Z4U", // Naver client key
+  //   callbackUrl: `https://jourgeois.com/api/member/login/naver/redirect`,
+  //   callbackHandle: true,
+  //   isPopup: true,
+  // });
+  // naverLogin.init();
+  // naverLogin.getLoginStatus(function (status: any) {
+  //   if (status) {
+  //     const email = naverLogin.user.getEmail();
+  //     const name = naverLogin.user.getName();
+  //     const profileImage = naverLogin.user.getProfileImage();
+  //     const birthday = naverLogin.user.getBirthday();
+  //     const uniqId = naverLogin.user.getId();
+  //     const age = naverLogin.user.getAge();
+  //     console.log(email);
+  //     console.log(name);
+  //     console.log(profileImage);
+  //     console.log(birthday);
+  //     console.log(uniqId);
+  //     console.log(age);
+  //   } else {
+  //     console.log("AccessToken이 올바르지 않습니다.");
+  //   }
+  // });
+  // naverLogin.reprompt();
 };
+
+// const clickNaver = () => {
+//   var client_id = 'D0B_Xn9oGT7I_66h9Z4U';
+//   var callbackUrl = 'https://jourgeois.com/api/member/login/naver/redirect';//서버 주소
+//   var url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + callbackUrl + '&state=1234';
+//   window.location.replace(url);
+// }
 </script>
 
 <style scoped lang="scss">

f57b5ed42872523f485e6395af8cda72f762f7ee
고승효 <shko0524@naver.com>
2022-08-12 17:46:05 +0900

    [S07P12B102-430] 회원관리: 커스텀칵테일 탭 마크업

--- frontend/src/components/profile/TheCustomCocktailPostList.vue
+++ frontend/src/components/profile/TheCustomCocktailPostList.vue
@@ -1,11 +1,27 @@
 <template>
-  <h4>
-    커스텀칵테일
-  </h4>
+  <p>커스텀</p>
+  <the-custom-cocktail-post-item
+    v-for="(post, id) in userPostListData"
+    :key="`main-${id}`"
+    :post="post"
+  ></the-custom-cocktail-post-item>
 </template>
 
 <script setup lang="ts">
+import TheCustomCocktailPostItem from '@/components/profile/TheCustomCocktailPostItem.vue';
+import { computed, onMounted } from "vue";
+import { useRoute } from 'vue-router';
+import { useStore } from "vuex";
+const route = useRoute()
+const store = useStore();
 
+const userPostListData = computed(
+  () => store.getters["profileDesc/getCurrentUserPostCommunity"]
+)
+onMounted(() => {
+  store.dispatch("profileDesc/getCurrentUserData", route.params.userId )
+  store.dispatch("profileDesc/getCurrentUserPostCustom", route.params.userId )
+});
 </script>
 
 <style scoped lang="ts">

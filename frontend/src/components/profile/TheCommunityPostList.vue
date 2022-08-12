9322e5715650c401d5145ed367530d1c5917f32a
박승훈 <tmdgns1126@naver.com>
2022-08-13 04:02:15 +0900

    [S07P12B102-407] Feat. 홈: 실시간 급상승 검색 랭킹 섹션 API 연결

--- frontend/src/components/profile/TheCommunityPostList.vue
+++ frontend/src/components/profile/TheCommunityPostList.vue
@@ -1,25 +1,23 @@
 <template>
-  <h4>
-    일반
-  </h4>
+  <h4>일반</h4>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed, onMounted } from "vue";
+import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 const route = useRoute();
 const store = useStore();
 
-const communityPost = computed(() => 
-  store.getters["profileDesc/getCurrentUserPostCommunity"]
-)
-console.log(communityPost.value)
+const communityPost = computed(
+  () => store.getters["profileDesc/getCurrentUserPostCommunity"]
+);
 onMounted(() => {
-  store.dispatch("profileDesc/getCurrentUserPostCommunity", route.params.userId )
+  store.dispatch(
+    "profileDesc/getCurrentUserPostCommunity",
+    route.params.userId
+  );
 });
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>

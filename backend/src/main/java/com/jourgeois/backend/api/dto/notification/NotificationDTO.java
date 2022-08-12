8a7dcfbc4f09629cde78ca2b264822c72a703426
Psalmist Kim <dss02094@gmail.com>
2022-08-12 11:54:51 +0900

    [S07P12B102-443] Feat. 피드 - 뉴스피드 : BE / [파일 업로드] 파일 업로드 시에 파일명에 특수문자 제거

--- backend/src/main/java/com/jourgeois/backend/api/dto/notification/NotificationDTO.java
+++ backend/src/main/java/com/jourgeois/backend/api/dto/notification/NotificationDTO.java
@@ -12,6 +12,8 @@ public class NotificationDTO {
     // FOLLOW, LIKE, COMMENT
     private NotificationType type;
 
+    private Long postId;
+
     // 상대방 정보
     private Long uid;
     private Boolean isRead;
